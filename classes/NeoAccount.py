0b77084edef70460ce052ba85ec8cdc39096e121
txtsd <thexerothermicsclerodermoid@gmail.com>
2016-08-12 13:20:51 +0530

    Update HTTP headers

--- classes/NeoAccount.py
+++ classes/NeoAccount.py
@@ -14,7 +14,7 @@ class NeoAccount:
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.7,ja;q=0.3',
+        'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate',
         'Dnt': '1'
     }
