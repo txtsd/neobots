18126214f58cabdc32e1370bc420d47155500d8e
txtsd <thexerothermicsclerodermoid@gmail.com>
2016-08-12 17:59:22 +0530

    Remove DNT from default HTTP headers

--- classes/NeoAccount.py
+++ classes/NeoAccount.py
@@ -16,7 +16,6 @@ class NeoAccount:
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate',
-        'Dnt': '1'
     }
 
     def __init__(self, username, password, proxy=""):
