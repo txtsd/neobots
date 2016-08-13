81298e1410caf0b9346829f1036129fcbd1a50ee
txtsd <thexerothermicsclerodermoid@gmail.com>
2016-08-13 21:19:51 +0530

    Disable commandline arguments

--- neobots.py
+++ neobots.py
@@ -10,14 +10,5 @@ neouser = ''  # Neopets Username
 neopass = ''  # Neopets Password
 proxyaddress = ''  # Optional proxy eg. '127.0.0.1:8888', leave at '' for none
 
-if len(sys.argv) == 4:
-    neouser = sys.argv[1]  # Get args from commandline
-    neopass = sys.argv[2]  # Get args from commandline
-    proxyaddress = sys.argv[3]  # Get args from commandline
-elif len(sys.argv) == 3:
-    neouser = sys.argv[1]  # Get args from commandline
-    neopass = sys.argv[2]  # Get args from commandline
-
-
 account = NeoAccount(neouser, neopass, proxyaddress)
 account.login()
